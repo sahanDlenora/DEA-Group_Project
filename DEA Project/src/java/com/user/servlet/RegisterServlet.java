36b290cf94ddab201eb9d9816e801ec14ed759da
sahanDlenora <140850822+sahanDlenora@users.noreply.github.com>
2024-04-01 23:40:46 +0530

    Admin

--- DEA Project/src/java/com/user/servlet/RegisterServlet.java
+++ DEA Project/src/java/com/user/servlet/RegisterServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -27,40 +28,49 @@ public class RegisterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         //processRequest(request, response);
-        
+
         try {
-            
-            String name=request.getParameter("fullname");
-            String email=request.getParameter("email");
-            String phn=request.getParameter("phnumber");
-            String psw=request.getParameter("password");
-            String check=request.getParameter("check");
-            
+
+            String name = request.getParameter("fullname");
+            String email = request.getParameter("email");
+            String phn = request.getParameter("phnumber");
+            String psw = request.getParameter("password");
+            String check = request.getParameter("check");
+
             //System.out.println(name+" "+email+" "+phn+" "+psw+" "+check);
-            
-            User us=new User();
-            
+            User us = new User();
+
             us.setName(name);
             us.setEmail(email);
             us.setPhno(phn);
             us.setPassword(psw);
             
-            UserDAOImpl dao=new UserDAOImpl(DBConnect.getConn());
-            
-            boolean f= dao.userRegister(us);
-            
-            if(f){
-                System.out.println("User register success..");
+            HttpSession session=request.getSession();
+
+            if (check != null) {
+                UserDAOImpl dao = new UserDAOImpl(DBConnect.getConn());
+
+                boolean f = dao.userRegister(us);
+
+                if (f) {
+                    //System.out.println("User register success.."); 
+                    
+                    session.setAttribute("succMsg","Registration Successfully..");
+                    response.sendRedirect("register.jsp");
+                } else {
+                   // System.out.println("Something wrong on server..");
+                   
+                   session.setAttribute("failedMsg","Something wrong on server..");
+                   response.sendRedirect("register.jsp");
+                }
             }else{
-                System.out.println("Something wrong on server..");
+                session.setAttribute("failedMsg","Please check agree Terms & Conditions ");
+                response.sendRedirect("register.jsp");
             }
-            
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    
 
 }
---
+++ DEA Project/web/admin/home.jsp
@@ -0,0 +1,14 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Admin : Home</title>
+        <%@include file="allCss.jsp" %>
+    </head>
+    <body>
+        <%@include file="navbar.jsp" %>
+        <h1>Admin:Home</h1>
+        <%@include file="footer.jsp" %>
+    </body>
+</html>
