a6b71b8e121ef01c78b00360886785f53cfbf18b
supuni001 <136358795+supuni001@users.noreply.github.com>
2024-04-28 14:12:45 +0530

    Update Cart.java

--- DEA Project/src/java/net/javaguides/registration/model/Cart.java
+++ DEA Project/src/java/net/javaguides/registration/model/Cart.java
@@ -1,14 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package net.javaguides.registration.model;
 
-/**
- *
- * @author User
- */
 public class Cart {
+    private int cart_id;
+    private int item_id;
+    private int user_id;
+    private String item_name;
+    private double price;
+    private double total_price;
+
+
+    public int getCart_id() {
+        return cart_id;
+    }
+
+    public void setCart_id(int cart_id) {
+        this.cart_id = cart_id;
+    }
+
+    public int getItem_id() {
+        return item_id;
+    }
+
+    public void setItem_id(int item_id) {
+        this.item_id = item_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getItem_name() {
+        return item_name;
+    }
+
+    public void setItem_name(String item_name) {
+        this.item_name = item_name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getTotal_price() {
+        return total_price;
+    }
+
+    public void setTotal_price(double total_price) {
+        this.total_price = total_price;
+    }
+      
     
 }
