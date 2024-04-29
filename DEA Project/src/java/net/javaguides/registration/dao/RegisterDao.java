4ec2597a826cfc36b6c2b066dc6196ea8e9e5b8c
Diluni Sewwandi <136349702+hdssandarekha@users.noreply.github.com>
2024-04-29 15:14:55 +0530

    Update RegisterDao.java

--- DEA Project/src/java/net/javaguides/registration/dao/RegisterDao.java
+++ DEA Project/src/java/net/javaguides/registration/dao/RegisterDao.java
@@ -52,4 +52,56 @@ public class RegisterDao {
         
         return status;
     }
+     public boolean checkPassword(int id,String ps)
+    {
+        boolean f =false;
+        PreparedStatement pst;
+        try
+        {
+            String sql ="SELECT * FROM user WHERE id=? AND password=?";
+            pst =conn.PrepareStatement(sql);
+            
+            pst.setInt(1,id);
+            pst.setString(2,ps);
+            
+            ResultSet rs = pst.executeQuery();
+            while(rs.next())
+            {
+                f = true;
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            
+        }
+        return f;
+    }
+    public boolean UpdateProfile(User us )
+    {
+          Connection conn= DbConnect.getConn();
+          boolean f =false;
+        
+        try {
+            String sql =  "UPDATE user SET name=?,email=?,phone_number=? WHERE id=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, us.getName());
+            ps.setString(2,us.getEmail());
+            ps.setString(3, us.getPhone());
+            ps.setInt(4,us.getId());
+           
+            int i = ps.executeUpdate();
+            if(i==1)
+            {
+                f = true;
+            }
+            ps.executeUpdate();
+              
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+          
+        }
+      
+        return false;
+    }
 }
